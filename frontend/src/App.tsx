89e694edd58b62e26127f8eb0739beb75a915e51
Warron Yiang Wai Loon <warronyiang@icloud.com>
2025-03-19 17:10:27 +0800

    fix: error basename on githubpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"webpage"}>
         <BaseLayout />
     </BrowserRouter>
   )
